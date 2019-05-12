77e3e33252c69a42d0c4b7f70d1ad18d62de16ad
sobhanniroula <sobhan_niroula@yahoo.com>
2019-05-12 14:25:27 +0300

    HTML & CSS implemented (almost)

---
+++ script.js
@@ -0,0 +1,11 @@
+first();
+
+
+
+
+// initial function that gets executed when a new game begins
+function first() {
+    document.getElementById('submit-area').style.display = 'none';
+    document.querySelector('#winner-area').style.display = 'none';
+   
+}
