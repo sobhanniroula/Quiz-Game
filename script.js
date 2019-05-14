84268a9b6680981174ae43683cf6520ff8316d18
sobhanniroula <sobhan_niroula@yahoo.com>
2019-05-14 12:08:57 +0300

    checkAnswer function works, and score is updated successfully

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var score, optionAlphabets, answerButtons;
+var score, optionAlphabets, answerButtons, answ;
 
 optionAlphabets = ['A', 'B', 'C', 'D'];
 answerButtons = [document.getElementById('btn-1'), document.getElementById('btn-2'), document.getElementById('btn-3'), document.getElementById('btn-4')];
@@ -12,8 +12,6 @@ function first() {
     score = 0;
     document.getElementById('submit-area').style.display = 'none';
     document.getElementById('winner-area').style.display = 'none';
-    
-   
 }
 
 
@@ -24,11 +22,22 @@ function Question(question, answers, correctAnswer) {
     this.correctAnswer = correctAnswer;
 }
 
-Question.prototype.displayQuestion = function() {
+// How to display questions with options on the screen?: (Here, when a question is loaded, the answers in it are assigned to each buttons. Also, the button values are assigned with the same data.)
+Question.prototype.displayQuestion = function () {
     document.getElementById('form-header').textContent = this.question;
-    
-    for (opt = 0; opt < optionAlphabets.length; opt++){
-        answerButtons[opt].textContent = optionAlphabets[opt] + ':     ' + this.answers[opt];
+
+    for (opt = 0; opt < optionAlphabets.length; opt++) {
+        answerButtons[opt].textContent = optionAlphabets[opt] + ': ' + this.answers[opt];
+        answerButtons[opt].value = this.answers[opt];
+    }
+}
+
+// Checking the answers:
+Question.prototype.checkAnswer = function (ans) {
+    if (ans === this.answers[this.correctAnswer]) {
+        score++;
+    } else {
+        score = score;
     }
 }
 
@@ -55,7 +64,7 @@ q8 = new Question('What sport does Sachin Tendulkar represent?', ['Football', 'C
 
 q9 = new Question('What is the currency of Australia?', ['Pound', 'Rupees', 'Dollar', 'Ringgit'], 2);
 
-q10 = new Question('What invented Computer Mouse?', ['Douglas Engelbart', 'Alexander Graham Bell', 'Thomas Alva Edison', 'Charles Babbage'], 0);
+q10 = new Question('What invented Computer Mouse ? ', ['Douglas Engelbart ', 'Alexander Graham Bell ', 'Thomas Alva Edison ', 'Charles Babbage '], 0);
 // ----------------------------------------------
 
 
@@ -66,13 +75,29 @@ var n = Math.floor(Math.random() * questions.length);
 questions[n].displayQuestion();
 
 
+// what happens if any button is clicked, among all 4 buttons?:
+for (var j = 0; j < optionAlphabets.length; j++){
+    var elem = document.getElementById('btn-' + (j + 1));
+    elem.addEventListener('click', function(elem) {        
+        answ = elem.srcElement.attributes.value.textContent;
+        questions[n].checkAnswer(answ);
+        nextQuestion();
+    });
+}
+
+
+// Time for next question:
+function nextQuestion() {
+    
+}
+  
 
 
 
 
 
 
-
+       
 
 
 
@@ -92,5 +117,5 @@ questions[n].displayQuestion();
 
 // What happens when we hit the Submit button?:
 function submitScore() {
-    document.getElementById('winner-area').style.display = 'block';
+                document.getElementById('winner-area').style.display = 'block';
 }
